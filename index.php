3b4a39731461f45b560bf102272cc1144dc1b7c7
Krzysztof Drzazga <drzazga_krzysztof@o2.pl>
2018-06-01 18:33:48 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head title="Przykładowa strona">
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <div class="header">
+
+        </div>
+        <div class="content">
+            <?php
+            ?>
+        </div>
+
+    </body>
+
+
+
+</html>
+
+
