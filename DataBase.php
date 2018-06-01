e45e6d89a941d7b3b7cdea24a1665221df884d8a
Dominik <dominikfijalek@gmail.com>
2018-06-01 20:45:16 +0200

    added database class

--- DataBase.php
+++ DataBase.php
@@ -1,7 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Dominik
- * Date: 2018-06-01
- * Time: 20:17
- */
+
+class Baza {
+    private $mysqli;
+
+    public function __construct($serwer, $user, $pass, $baza, $mysqli) {
+        $this->mysqli = new mysqli($serwer, $user, $pass, $baza);
+
+        if ($this->mysqli->connect_errno) {
+            printf("Nie udało sie połączenie z serwerem: %s\n", $mysqli->connect_error);
+            exit();
+        }
+
+        if ($this->mysqli->set_charset("utf8")) {
+        }
+    }
+    function __destruct() {
+        $this->mysqli->close();
+    }
+
+    public function select($sql) {
+        $res = $this->mysqli->query($sql);
+        return $res;
+    }
+    public function insert($sql) {
+        $this->mysqli->query($sql);
+    }
+    public function delete($sql) {
+        $this->mysqli->query($sql);
+    }
+}
+?>
